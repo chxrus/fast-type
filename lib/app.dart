ef330182ef40da4ff18ae62e0061bbad5aa11888
chxrus <lukovkin.ark@gmail.com>
2024-08-21 22:37:12 +0300

    fix: organize imports

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
-import 'package:fast_type/features/type/view/view.dart';
-import 'package:fast_type/repositories/sentence/i_sentence_repository.dart';
-import 'package:fast_type/repositories/sentence/sentence_repository.dart';
+import 'package:fast_type/features/type/type.dart';
+import 'package:fast_type/repositories/sentence/sentence.dart';
 import 'package:fast_type/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
